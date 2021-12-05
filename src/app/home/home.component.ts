4347426287c0e50ffa1e6a31656075903a635073
King-Martinien <martinienfokoue@yahoo.com>
2021-12-05 13:42:13 +0100

    Home meta test:2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,23 +10,25 @@ export class HomeComponent implements OnInit {
   constructor(private _title: Title, private _meta: Meta) {}
 
   ngOnInit(): void {
-    this._title.setTitle('Meta Test - Titre modifié dans la route Home');
-    this._meta.updateTag({
-      name: 'description',
-      content:
-        'Description modifiée dans la route Home par le King Martinien hahahaha ça marche',
-    });
-    this._meta.updateTag({
-      name: 'title',
-      content: 'Le King a modifié le titre de la meta dans la route home',
-    });
-    this._meta.updateTag({
-      property: 'og:image',
-      content: `assets/images/nike-chaussure.jpg`,
-    });
-    this._meta.updateTag({
-      property: 'twitter:image',
-      content: `assets/images/nike-chaussure.jpg`,
-    });
+    setTimeout(() => {
+      this._title.setTitle('Meta Test - Titre modifié dans la route Home');
+      this._meta.updateTag({
+        name: 'description',
+        content:
+          'Description modifiée dans la route Home par le King Martinien hahahaha ça marche',
+      });
+      this._meta.updateTag({
+        name: 'title',
+        content: 'Le King a modifié le titre de la meta dans la route home',
+      });
+      this._meta.updateTag({
+        property: 'og:image',
+        content: `assets/images/nike-chaussure.jpg`,
+      });
+      this._meta.updateTag({
+        property: 'twitter:image',
+        content: `assets/images/nike-chaussure.jpg`,
+      });
+    }, 500);
   }
 }
